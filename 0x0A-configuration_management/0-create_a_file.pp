1414a1192a092dc005b02f7f0c67a138bd51ef77
eufot60 <eufot60@gmail.com>
2024-05-03 21:36:13 -0700

    g

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-file{'/tmp/school':
-ensure  => file.
-conetnt => 'i love Puppet'.
-node    => '07744'.
-owner   => 'www-data'.
-group   => 'www-data'.
+file{ '/tmp/school':
+  ensure   => file,
+  conetnt  => 'i love Puppet',
+  node     => '07744',
+  owner    => 'www-data',
+  group    => 'www-data',
 }
